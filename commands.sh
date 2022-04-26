9b6224ba7356284052318b01e65862afc1e91af7
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2022-04-26 15:45:52 +0200

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,8 @@
+echo "There is a change!"
+echo "Going to changed directory"
+cd /home/Zafar_Final_CD_Assignment
+echo "Printing working directory"
+pwd
+echo "restart application"
+systemctl restart /Zafar_Final_CD_Assignment
+echo "You're up to date and application is restarted"
